71b418a504ac987287002e2aa532d35f1b471ae2
kreipikc <basmenov5kir@gmail.com>
2025-01-20 10:22:52 +0300

    init

---
+++ 222_Darinskaya_Basmanov/Helper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Converters;
+
+namespace _222_Darinskaya_Basmanov
+{
+    internal class Helper
+    {
+        static public double sh(double x)
+        {
+            return (Math.Pow(Math.E, x) - Math.Pow(Math.E, -x)) / 2.0;
+        }
+
+        static public double Function(double f, double x, double y)
+        {
+            if (x * y > 0)
+            {
+                return Math.Pow((f + y), 2) - Math.Sqrt(f * y);
+            }
+            else if (x * y < 0)
+            {
+                return Math.Pow((f + y), 2) + Math.Sqrt(Math.Abs(f * y));
+            }
+            else if (x * y == 0)
+            {
+                return Math.Pow((f + y), 2) + 1;
+            }
+            return -1;
+        }
+    }
+}
--- 222_Darinskaya_Basmanov/MainWindow.xaml.cs
+++ 222_Darinskaya_Basmanov/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,88 @@ namespace _222_Darinskaya_Basmanov
     /// </summary>
     public partial class MainWindow : Window
     {
+        RadioButton ActiveButton;
+
         public MainWindow()
         {
             InitializeComponent();
+            ActiveButton = RadioButton1;
+        }
+
+        private void Calculate(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double x = Convert.ToDouble(TextBox_X.Text);
+                double y = Convert.ToDouble(TextBox_Y.Text);
+
+                if (ActiveButton == RadioButton1)
+                {
+                    double result = Helper.Function(Helper.sh(x), x, y);
+                    Result.Text = result.ToString();
+                }
+                else if (ActiveButton == RadioButton2)
+                {
+                    double result = Helper.Function(Math.Pow(x, 2), x, y);
+                    Result.Text = result.ToString();
+                }
+                else if (ActiveButton == RadioButton3)
+                {
+                    double result = Helper.Function(Math.Pow(Math.E, x), x, y);
+                    Result.Text = result.ToString();
+                }
+            }
+            catch (FormatException error)
+            {
+                MessageBox.Show(
+                            $"{error.Message}",
+                            "Ошибка",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Error
+                        );
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(
+                            $"{error.Message}",
+                            "Ошибка",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Error
+                        );
+            }
+        }
+
+        private void Clear_All(object sender, RoutedEventArgs e)
+        {
+            ActiveButton = RadioButton1;
+            ActiveButton.IsChecked = true;
+
+            TextBox_X.Text = string.Empty;
+            TextBox_Y.Text = string.Empty;
+            Result.Text = string.Empty;
+        }
+
+        private void RadioButton1_Checked(object sender, RoutedEventArgs e)
+        {
+            ActiveButton = RadioButton1;
+        }
+
+        private void RadioButton2_Checked(object sender, RoutedEventArgs e)
+        {
+            ActiveButton = RadioButton2;
+        }
+
+        private void RadioButton3_Checked(object sender, RoutedEventArgs e)
+        {
+            ActiveButton = RadioButton3;
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Вы точно хотите выйти?", "Выход", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.No)
+                e.Cancel = true;
+            base.OnClosing(e);
         }
     }
 }
